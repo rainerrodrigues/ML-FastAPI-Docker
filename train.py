0090211a455a4e6f78929a1590204d9dd026f4bf
Rainer Rodrigues <rainerrodrigues16@gmail.com>
2026-01-17 07:37:44 +0000

    Performed mlflow on model and updated model with n_estimators=200

--- train.py
+++ train.py
@@ -1,13 +1,45 @@
+import mlflow
+import mlflow.sklearn
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+# Load data
 X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
+# Experiment name
+mlflow.set_experiment("iris-classification")
 
-model = RandomForestClassifier(n_estimators=100, max_depths=5)
-model.fit(X,y)
+with mlflow.start_run():
+    # Params
+    n_estimators = 200
+    max_depth = 4
 
-joblib.dump(model,"app/model.joblib")
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
 
-print("Model trained and saved.")
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+
+    # Log params & metrics
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_metric("accuracy", acc)
+
+    # Log model
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        registered_model_name="IrisClassifier"
+    )
+
+    print(f"Accuracy: {acc}")
+
